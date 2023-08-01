e56db608e6ca7f54ccc8550de7e5a73349e5d445
casagrande7 <joaofootz10@gmail.com>
2023-08-01 15:13:30 -0300

    Exercícios Dez

---
+++ app/Http/Controllers/ExercicioOnzeController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ExercicioOnzeController extends Controller
+{
+    
+}
--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,22 @@
 <?php
 
+use App\Http\Controllers\ExercicioCincoController;
+use App\Http\Controllers\ExercicioDezController;
+use App\Http\Controllers\ExercicioDozeController;
+use App\Http\Controllers\ExercicioNoveController;
+use App\Http\Controllers\ExercicioOitoController;
+use App\Http\Controllers\ExercicioQuatrooController;
+use App\Http\Controllers\ExercicioSeisController;
+use App\Http\Controllers\ExercicioSeteController;
+use App\Http\Controllers\ExercicioTresController;
+use App\Http\Controllers\ExercicioTrezeController;
+use App\Http\Controllers\ExercicioUmController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
 
-Route::post('exercicio/um', [ExercicioUmController::class, 'multiplicar']);
+Route::post('exercicio/um', [ExercicioUmController::class,   'multiplicar']);
 
 Route::post('exercicio/dois', [ExercicioDoisController::class, 'retornarMaior']);
 
@@ -22,4 +33,11 @@ Route::get('exercicio/sete', [ExercicioSeteController::class, 'exibirNumeros']);
 Route::post('exercicio/oito', [ExercicioOitoController::class, 'tabuada']);
 
 Route::post('exercicio/nove', [ExercicioNoveController::class, 'divisao']);
+
+Route::post('exercicio/dez', [ExercicioDezController::class, 'numerosImpares']);
+
+Route::post('exercicio/doze', [ExercicioDozeController::class, 'numerosDecrescentes']);
+
+Route::post('exercicio/treze', [ExercicioTrezeController::class, 'restofive']);
+
 
