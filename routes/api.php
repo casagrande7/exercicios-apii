1cd39b8f7e9545524eee7e6b4f1ede186bc57f59
casagrande7 <joaofootz10@gmail.com>
2023-08-01 09:54:10 -0300

    ExercicioDois

--- routes/api.php
+++ routes/api.php
@@ -7,4 +7,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('exercicio/um', [ExercicioUmController::class, 'multiplicar']);
 
-
+Route::post('exercicio/dois', [ExercicioDoisController::class, 'retornarMaior']);
