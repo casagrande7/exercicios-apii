c1a26098559ff52cfd938cc8ec9aa5f7cc9a5043
casagrande7 <joaofootz10@gmail.com>
2023-08-01 10:13:37 -0300

    ExercícioNove

--- routes/api.php
+++ routes/api.php
@@ -20,3 +20,6 @@ Route::post('exercicio/seis', [ExercicioSeisController::class, 'idade']);
 Route::get('exercicio/sete', [ExercicioSeteController::class, 'exibirNumeros']);
 
 Route::post('exercicio/oito', [ExercicioOitoController::class, 'tabuada']);
+
+Route::post('exercicio/nove', [ExercicioNoveController::class, 'divisao']);
+
